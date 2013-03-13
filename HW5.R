bce8ee07a90c6f295b77cbfbbbbe2b3e9c73ca4a
Olivia Lau <oliviaslau@gmail.com>
2013-03-13 10:21:36 -0700

    Parallel for reading in test files into R and sampling, working

--- HW5.R
+++ HW5.R
@@ -1,4 +1,32 @@
+##################
+#USEFUL FUNCTIONS#
+##################
+#detectCores()
+#makeCluster()
+#stopCluster()
+#clusterSetRNGStream()
+#clusterEvalQ()
+#clusterApplyLB()
+#parLapply()
+
 library(parallel)
 setwd("/Users/Olivia/Documents/STA 242/Assignment4/Data")
 detectCores()	#4
 cl = makeCluster(3, type = "FORK")
+
+#########
+#TESTING#
+#########
+n = 100000
+test = read.csv('2000.csv', header = TRUE, nrows = 1000)
+test1 = read.csv('2001.csv', header = TRUE, nrows = 1000)
+##Do this in parallel for all the years?
+clusterApply(cl, list(test, test1), function(x, n){x[sample(nrow(x), n, replace = TRUE),]}, n = 10)
+
+
+
+
+
+
+
+stopCluster(cl)
