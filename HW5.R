2d5fd4b58739f9629e9ab1aa984181901c859a88
Olivia Lau <oliviaslau@gmail.com>
2013-03-19 13:13:01 -0700

    Fixed sampling in R solely

--- HW5.R
+++ HW5.R
@@ -19,20 +19,44 @@ library(snow)
 library(RSQLite)
 Sys.setlocale(locale = "C")
 setwd("/Users/Olivia/Documents/STA 242/Assignment4/Data")
-detectCores()	#4
+#detectCores()	#4
 if(!exists("shell")){
 	shell = system
 }
 cl = makeCluster(3, type = "FORK")
 clusterSetRNGStream(cl)
 files = list.files()[1:22]
+
+
+nsamp = 100
+lineCount = read.table(textConnection(shell("wc -l [12]*.csv", intern = TRUE)), header = FALSE)
+mySamples = apply(lineCount, 1, function(x, n){
+	sample(2:x[1], nsamp, replace = TRUE)
+	}, nsamp)
+mydat = clusterApply(cl, 1:22, function(x, samples){
+	cat(readLines(files[x])[samples[[x]]], file = out[[x]], sep = "\n")
+}, mySamples)
+stopCluster(cl)
+
+n = 1000
 mydat = clusterApplyLB(cl, files, function(x){
 	lineCount = as.integer(strsplit(shell(paste("wc -l ", x, sep = ""), intern = TRUE), " ")[[1]][2])
-	n = .1 * lineCount
+	read.csv(textConnection(readLines(x)[sample(2:lineCount, n, replace = TRUE)]), header = FALSE)
+})
+
+mydat = clusterApplyLB(cl, files, function(x, n){
+	lineCount = as.integer(strsplit(shell(paste("wc -l ", x, sep = ""), intern = TRUE), " ")[[1]][2])
+	samp = sample(2:lineCount, n, replace = TRUE)
+	lineCmd = paste("sed -n '", paste(samp, "p", sep = "", collapse = ";"), "' ", x, sep = "")
+	read.csv(textConnection(shell(lineCmd, intern = TRUE)), header=FALSE)
+}, n)
+
+mydat = clusterApplyLB(cl, files, function(x, n){
+	lineCount = as.integer(strsplit(shell(paste("wc -l ", x, sep = ""), intern = TRUE), " ")[[1]][2])
 	samp = sample(2:lineCount, n, replace = TRUE)
 	lineCmd = paste("./looper.sh ", x, paste(samp, collapse = " "), sep = " ")
 	read.csv(textConnection(shell(lineCmd, intern = TRUE)), header=FALSE)
-})
+}, n)
 stopCluster(cl)
 mydat = do.call("rbind", mydat)
 colnames(mydat)<- strsplit(readLines("2001.csv", 1), ",")[[1]]
@@ -90,6 +114,22 @@ mydat = clusterApplyLB(cl, files, function(x, n){
 #implement in sqlite3
 dr = dbDriver("SQLite")
 con = dbConnect(dr, dbname = "airline")
-datSQL = dbSendQuery(con, "SELECT * FROM delays")
-fetch(datSQL) ##Now...somehow grab certain lines...hm...
-#randomforest!
+cl = makeCluster(3, type = "FORK")
+clusterSetRNGStream(cl)
+files = list.files()[1:22]
+mydat = clusterApplyLB(cl, files, function(x){
+	countQuery = paste("SELECT count(*) FROM delays WHERE year = ", substr(x, 1, 4), ";", sep = "")
+	countSQL = unlist(sqliteQuickSQL(con, countQuery))
+	n = .1*countSQL
+	samp = sample(1:countSQL, n, replace = TRUE)
+	samp = sort(samp)
+	datQuery = paste("SELECT * FROM delays WHERE year = ", substr(x, 1, 4), ";", sep = "")
+	datSQL = dbSendQuery(con, datQuery)
+	fetch(datSQL)[samp,] ##Now...somehow grab certain lines...hm...
+})
+
+sqliteCloseResult(con)
+stopCluster(cl)
+mydat = do.call("rbind", mydat)
+mydat = mydat[sample(1:nrow(mydat), nrow(mydat)),]
+mydat = mydat[!is.na(mydat$ArrTime), ]
